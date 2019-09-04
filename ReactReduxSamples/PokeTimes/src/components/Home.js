93a96d3474df4a448cc546d57403d07f908b91a9
Sriram Srinivasan <52806449+sriramincconsulting@users.noreply.github.com>
2019-09-04 19:27:37 +0530

    A complete React Sample covering all it's basic featuers from Routers to Redux

--- ReactReduxSamples/PokeTimes/src/components/Home.js
+++ ReactReduxSamples/PokeTimes/src/components/Home.js
@@ -1,21 +1,10 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 class Home extends Component {
-  state = {
-    posts: []
-  };
-  componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-      console.log(res);
-      this.setState({
-        posts: res.data.slice(0, 10)
-      });
-    });
-  }
   render() {
-    const { posts } = this.state;
+    const { posts } = this.props;
     const postlist = posts.length ? (
       posts.map(post => {
         return (
@@ -41,4 +30,9 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+export default connect(mapStateToProps)(Home);
